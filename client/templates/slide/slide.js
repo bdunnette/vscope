d3792cc5c99cb7e2758e04f4d5630857ceb6ab2a
Brian Dunnette <dunn0172@umn.edu>
2015-06-26 13:15:57 -0500

    adding basic slide view

--- client/templates/slide/slide.js
+++ client/templates/slide/slide.js
@@ -1,23 +1,45 @@
 /*****************************************************************************/
 /* Slide: Event Handlers */
 /*****************************************************************************/
-Template.Slide.events({
-});
+Template.Slide.events({});
 
 /*****************************************************************************/
 /* Slide: Helpers */
 /*****************************************************************************/
 Template.Slide.helpers({
+  slideUrl: function() {
+    console.log(this);
+  }
 });
 
 /*****************************************************************************/
 /* Slide: Lifecycle Hooks */
 /*****************************************************************************/
-Template.Slide.created = function () {
+Template.Slide.created = function() {};
+
+Template.Slide.rendered = function() {
+  $('#map').css({
+    height: $(window).innerHeight() + 'px'
+  });
+
+  var map = L.map('map').setView([0,0], 3);
+  var slideTiles = L.tileLayer('/images/blank.png', {id: 'slideTiles', attribution: 'Images &copy; Regents of the University of Minnesota', maxZoom: 8});
+  slideTiles.addTo(map);
+
+  var query = Slides.find({_id: Router.current().params._id});
+  query.observe({
+    added: function (document) {
+      slideTiles.setUrl(document.slideUrl + '/{z}/{y}/{x}.jpg');
+      console.log(slideTiles);
+    }
+  });
+
+  $(window).resize(function() {
+    $('#map').css({
+      height: $(window).innerHeight() + 'px'
+    });
+  });
 };
 
-Template.Slide.rendered = function () {
-};
-
-Template.Slide.destroyed = function () {
+Template.Slide.destroyed = function() {
 };
