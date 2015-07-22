deba288a67d391da2a7ecba7dff995274d4d9fed
Brian Dunnette <dunn0172@umn.edu>
2015-07-22 15:55:56 -0500

    tidying code

--- client/templates/slide/slide.js
+++ client/templates/slide/slide.js
@@ -22,21 +22,26 @@ Template.Slide.rendered = function() {
     height: $(window).innerHeight() + 'px'
   });
 
-  var map = L.map('map').setView([0,0], 3);
-  var slideTiles = L.tileLayer('/images/blank.png', {id: 'slideTiles', attribution: 'Images &copy; Regents of the University of Minnesota', maxZoom: 8});
+  var map = L.map('map').setView([0, 0], 3);
+  var slideTiles = L.tileLayer('/images/blank.png', {
+    id: 'slideTiles',
+    attribution: 'Images &copy; Regents of the University of Minnesota',
+    maxZoom: 8
+  });
   slideTiles.addTo(map);
 
-  var slideQuery = Slides.find({_id: Router.current().params._id});
+  var slideQuery = Slides.find({
+    _id: Router.current().params._id
+  });
   slideQuery.observe({
-    added: function (document) {
+    added: function(document) {
       slideTiles.setUrl(document.slideUrl + '/{z}/{y}/{x}.jpg');
     }
   });
 
   var drawnItems = L.featureGroup().addTo(map);
-  console.log(drawnItems);
-
-  if (Meteor.userId()){
+  
+  if (Meteor.userId()) {
     map.addControl(new L.Control.Draw({
       draw: {
         polyline: false,
@@ -52,17 +57,14 @@ Template.Slide.rendered = function() {
   }
 
   map.on('draw:created', function(event) {
-      var layer = event.layer;
-      console.log(event.layer);
-      console.log(event.layerType);
-      console.log(drawnItems);
-      var feature = {
-        options: event.layer.options,
-        layerType: event.layerType,
-        slide: Router.current().params._id,
-        user: Meteor.userId()
-      };
-      switch (event.layerType) {
+    var layer = event.layer;
+    var feature = {
+      options: event.layer.options,
+      layerType: event.layerType,
+      slide: Router.current().params._id,
+      user: Meteor.userId()
+    };
+    switch (event.layerType) {
       case 'marker':
         feature.latlng = event.layer._latlng;
         break;
@@ -70,53 +72,49 @@ Template.Slide.rendered = function() {
         feature.latlng = event.layer._latlng;
         feature.radius = event.layer._mRadius;
         break;
-      }
-      console.log(feature);
-      Markers.insert(feature);
-    });
+    }
+    Markers.insert(feature);
+  });
 
 
-    map.on('draw:deleted', function(event) {
-      console.log(event);
-      console.log(event.layers._layers);
-      for (var l in event.layers._layers) {
-        console.log(l);
-        Markers.remove({_id: l});
-      }
-    });
+  map.on('draw:deleted', function(event) {
+    for (var l in event.layers._layers) {
+      Markers.remove({
+        _id: l
+      });
+    }
+  });
 
-    var markerQuery = Markers.find({slide: Router.current().params._id});
-    console.log(markerQuery.fetch());
-    markerQuery.observe({
-      added: function(document) {
-        console.log(document);
-        switch (document.layerType) {
+  var markerQuery = Markers.find({
+    slide: Router.current().params._id
+  });
+
+  markerQuery.observe({
+    added: function(document) {
+      switch (document.layerType) {
         case 'marker':
-          var marker = L.marker(document.latlng);
-          marker._leaflet_id = document._id;
-          marker.addTo(drawnItems);
+          var feature = L.marker(document.latlng);
           break;
         case 'circle':
-        var circle = L.circle(document.latlng, document.radius);
-        circle._leaflet_id = document._id;
-        circle.addTo(drawnItems);
+          var feature = L.circle(document.latlng, document.radius);
           break;
-        }
-      },
-      removed: function(oldDocument) {
-        console.log(oldDocument);
-        layers = map._layers;
-        var key, val;
-        for (key in layers) {
-          val = layers[key];
-          if (val._latlng) {
-            if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
-              map.removeLayer(val);
-            }
+      }
+      feature._leaflet_id = document._id;
+      feature.addTo(drawnItems);
+    },
+    removed: function(oldDocument) {
+      layers = map._layers;
+      var key, val;
+      for (key in layers) {
+        val = layers[key];
+        if (val._latlng) {
+          if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
+            map.removeLayer(val);
           }
         }
       }
-    });
+    }
+  });
 
   $(window).resize(function() {
     $('#map').css({
@@ -125,5 +123,4 @@ Template.Slide.rendered = function() {
   });
 };
 
-Template.Slide.destroyed = function() {
-};
+Template.Slide.destroyed = function() {};
