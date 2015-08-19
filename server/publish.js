8f451c8da2757e9e9d33eacc6b360a617a74d28b
Brian Dunnette <dunn0172@umn.edu>
2015-08-19 09:45:14 -0500

    adding case model

--- server/publish.js
+++ server/publish.js
@@ -5,18 +5,27 @@
  */
 
 
-Meteor.publish('slides', function (/* args */) {
+Meteor.publish('slides', function( /* args */ ) {
   return Slides.find();
 });
 
-Meteor.publish('slide', function (slideId) {
-  return Slides.find({_id: slideId});
+Meteor.publish('slide', function(slideId) {
+  return Slides.find({
+    _id: slideId
+  });
 }, {
   url: "publications/slide/:0"
 });
 
-Meteor.publish('slideMarkers', function (slideId) {
-  return Markers.find({slide: slideId});
+Meteor.publish('slideMarkers', function(slideId) {
+  return Markers.find({
+    slide: slideId
+  });
 }, {
   url: "publications/slideMarkers/:0"
+});
+
+
+Meteor.publish('cases', function () {
+  return Cases.find();
 });
